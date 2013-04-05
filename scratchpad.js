260ebe476717b156c71be83911a3e48843f09b54
JejakaPemalu <do.u.want.my.current.email@gmail.com>
2013-04-06 04:01:48 +0800

    Socket error due to missing @require base64 and missing request variables on new chat

--- scratchpad.js
+++ scratchpad.js
@@ -10,9 +10,10 @@
 // @include     http://w3.tbd.my/*
 // @include     https://w3.tbd.my/*
 // @exclude			http*://w3.tbd.my/xmlhttp.php*
-// @require     http://chat.tbd.my/tbd/pushstream.js
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @version         1.0.30
+// @require			http://chat.tbd.my/tbd/jquery.base64.min.js
+// @require     http://chat.tbd.my/tbd/pushstream.js
+// @version     1.0.44
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_log
@@ -26,6 +27,7 @@ if (typeof GM_log === 'undefined'){
 		console.log(msg);
 	}
 }
+
 if (typeof GM_xmlhttprequest === 'undefined'){
 	var GM_xmlhttprequest = function (params) {
    	    var req = new XMLHttpRequest();
@@ -43,10 +45,11 @@ if (typeof GM_xmlhttprequest === 'undefined'){
 }
 
 
-//TBDRequest Represents what is displayed at chatbox.
+
 //Initialize of the bot
 var GM_TBD_init = function(){
 	GM_log('Initialized');
+	//TBDRequest Represents what is displayed at chatbox.
 	var TBDRequest = new function (){
 		this.url='chat.tbd.my';
 		this.port = '80';                                                                                                          
@@ -86,32 +89,9 @@ var GM_TBD_init = function(){
 			GM_log('BeforeFilter :'+TBDRequest.message);
 		};
 	}
-		if (("WebSocket" in window)===true){    //Yes Got websocket
-				GM_log('Using socket');
-				var socket = new PushStream({
-					host: TBDRequest.url,
-					port: TBDRequest.port,
-					modes: "websocket",
-					channelsByArgument: true,
-					channelsArgument: 'channels'
-				});
-		//a = base64 msg
-		//b = msgID
-		//c = channel
-			socket.onmessage = function(a,b,c){
-				if (TBDRequest.lastid < b && c === TBDRequest.channel){//Make sure the msg isn't it
-					var d = TBDJQuery.parseJSON(TBDJQuery.base64.decode(a));
-					TBDRequest.message = d.shout_msg;
-					TBDResponse.beforeFilter(d);
-				}
-				TBDRequest.lastid = b;
-		  };		
-			socket.addChannel(TBDRequest.channel);
-			socket.connect();	
-	}else{//no got websocket
-		GM_log('No socket');
-		setInterval (function() {
-			TBDRequest.GET(TBDRequest.shout_url,function(doc) {
+	//HTTP get chat contents
+	var http_get_chat = function(){
+		TBDRequest.GET(TBDRequest.shout_url,function(doc) {
 						var d = doc.documentElement.innerHTML.split("<br>");
 						d = d[0].split("member.php?action=profile&amp;uid=");
 						var tmp = d[0].split('^'), 
@@ -124,11 +104,39 @@ var GM_TBD_init = function(){
 						TBDRequest.message = d[1];
 
 				if (TBDRequest.lastid < b ){
-					TBDResponse.beforeFilter(d[1]);
+					TBDResponse.beforeFilter();
 					TBDRequest.lastid = b;
 				}
 			});
-		},2000)
+	}
+		if (("WebSocket" in window)===true){    //Yes Got websocket
+				GM_log('Using socket');
+				http_get_chat();
+				var socket = new PushStream({
+					host: TBDRequest.url,
+					port: TBDRequest.port,
+					modes: "websocket",
+					channelsByArgument: true,
+					channelsArgument: 'channels'
+				});
+		//a = base64 msg
+		//b = msgID
+		//c = channel
+			socket.onmessage = function(a,b,c){
+				if (TBDRequest.lastid < b && c === TBDRequest.channel){//Make sure the msg isn't it
+					var d = jquery.parseJSON(jquery.base64.decode(a));
+					TBDRequest.message = d.shout_msg;
+					TBDRequest.username = jquery(d.uname).html();
+					TBDRequest.userid = d.uid;
+					TBDResponse.beforeFilter();
+				}
+				TBDRequest.lastid = b;
+		  };		
+			socket.addChannel(TBDRequest.channel);
+			socket.connect();	
+	}else{//no got websocket
+		GM_log('No socket');
+		setInterval (function(){http_get_chat()},2000)
 	}
 };
 
